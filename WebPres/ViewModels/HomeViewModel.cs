67459cd0bb3dc19819e681c0f700ae30bd8985cf
SoapDispenser <abhiram.kasu@gmail.com>
2024-04-06 01:15:45 -0600

    Added Icons and "Add New Flow" button without functionality

--- WebPres/ViewModels/HomeViewModel.cs
+++ WebPres/ViewModels/HomeViewModel.cs
@@ -1,8 +1,16 @@
+
+using System.Threading.Tasks;
 using AvaloniaHelpers.Navigation.MVVM;
+using CommunityToolkit.Mvvm.Input;
 
 namespace WebPres.ViewModels;
-internal class HomeViewModel : BaseViewModel
+internal partial class HomeViewModel : BaseViewModel
 {
     public string Title { get; } = "Your Flow(s)";
+    [RelayCommand]
+    public async Task CreateNewFlow()
+    {
+        
+    }
 
 }
